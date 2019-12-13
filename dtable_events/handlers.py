191c27a475179b0c30b97d4423bb348945d7a6b2
王健辉 <40563566+jianhw@users.noreply.github.com>
2019-12-13 16:47:52 +0800

    repair code

--- dtable_events/handlers.py
+++ dtable_events/handlers.py
@@ -4,10 +4,8 @@ import time
 import json
 from threading import Thread, Event
 
-from seaserv import ccnet_api
-
 from dtable_events.event_redis import event_redis
-from dtable_events.models import Activities, UserActivities
+from dtable_events.utils import save_user_activities
 from dtable_events.db import init_db_session_class
 
 logger = logging.getLogger(__name__)
@@ -23,50 +21,6 @@ def _redis_connection(config):
             time.sleep(5)
         else:
             return connection
-
-
-def _save_user_activities(session, event):
-    dtable_uuid = event['dtable_uuid']
-    row_id = event['row_id']
-    op_user = event['op_user']
-    op_type = event['op_type']
-
-    local_time = time.localtime(int(event['op_time']) / 1000)
-    op_time = time.strftime('%Y-%m-%d %H:%M:%S', local_time)
-
-    table_id = event['table_id']
-    table_name = event['table_name']
-    row_data = event['row_data']
-
-    detail_dict = dict()
-    detail_dict["table_id"] = table_id
-    detail_dict["table_name"] = table_name
-    detail_dict["row_data"] = row_data
-    detail = json.dumps(detail_dict)
-
-    activity = Activities(dtable_uuid, row_id, op_user, op_type, op_time, detail)
-    session.add(activity)
-    session.commit()
-
-    cmd = "SELECT to_user FROM dtable_share WHERE dtable_id=(SELECT id FROM dtables WHERE uuid=:dtable_uuid)"
-    user_list = [res[r'to_user'] for res in session.execute(cmd, {"dtable_uuid": dtable_uuid})]
-
-    cmd = "SELECT owner FROM workspaces WHERE id=(SELECT workspace_id FROM dtables WHERE uuid=:dtable_uuid)"
-    owner = [res[r'owner'] for res in session.execute(cmd, {"dtable_uuid": dtable_uuid})][0]
-
-    if '@seafile_group' not in owner:
-        user_list.append(op_user)
-    else:
-        group_id = int(owner.split('@')[0])
-        members = ccnet_api.get_group_members(group_id)
-        for member in members:
-            if member.user_name not in user_list:
-                user_list.append(member.user_name)
-
-    for user in user_list:
-        user_activity = UserActivities(activity.id, user, activity.op_time)
-        session.add(user_activity)
-    session.commit()
 
 
 class MessageHandler(Thread):
@@ -109,7 +63,7 @@ class EventHandler(Thread):
 
                     session = self._db_session_class()
                     try:
-                        _save_user_activities(session, event)
+                        save_user_activities(session, event)
                     except Exception as e:
                         logger.error(e)
                     finally:
--- dtable_events/utils.py
+++ dtable_events/utils.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import time
 import json
 import logging
 
 from sqlalchemy import desc
+from seaserv import ccnet_api
 
 from dtable_events.models import Activities, UserActivities
 
@@ -44,3 +46,47 @@ def get_user_activities(session, username, start, limit):
         logger.error(e)
 
     return [UserActivityDetail(activity) for activity in activities]
+
+
+def save_user_activities(session, event):
+    dtable_uuid = event['dtable_uuid']
+    row_id = event['row_id']
+    op_user = event['op_user']
+    op_type = event['op_type']
+
+    local_time = time.localtime(int(event['op_time']) / 1000)
+    op_time = time.strftime('%Y-%m-%d %H:%M:%S', local_time)
+
+    table_id = event['table_id']
+    table_name = event['table_name']
+    row_data = event['row_data']
+
+    detail_dict = dict()
+    detail_dict["table_id"] = table_id
+    detail_dict["table_name"] = table_name
+    detail_dict["row_data"] = row_data
+    detail = json.dumps(detail_dict)
+
+    activity = Activities(dtable_uuid, row_id, op_user, op_type, op_time, detail)
+    session.add(activity)
+    session.commit()
+
+    cmd = "SELECT to_user FROM dtable_share WHERE dtable_id=(SELECT id FROM dtables WHERE uuid=:dtable_uuid)"
+    user_list = [res[r'to_user'] for res in session.execute(cmd, {"dtable_uuid": dtable_uuid})]
+
+    cmd = "SELECT owner FROM workspaces WHERE id=(SELECT workspace_id FROM dtables WHERE uuid=:dtable_uuid)"
+    owner = [res[r'owner'] for res in session.execute(cmd, {"dtable_uuid": dtable_uuid})][0]
+
+    if '@seafile_group' not in owner:
+        user_list.append(op_user)
+    else:
+        group_id = int(owner.split('@')[0])
+        members = ccnet_api.get_group_members(group_id)
+        for member in members:
+            if member.user_name not in user_list:
+                user_list.append(member.user_name)
+
+    for user in user_list:
+        user_activity = UserActivities(activity.id, user, activity.op_time)
+        session.add(user_activity)
+    session.commit()
