2c02ad37729070d8d0b8e9c3ac9d689842eca6a6
王健辉 <40563566+jianhw@users.noreply.github.com>
2019-12-25 12:07:37 +0800

    get user activity

--- dtable_events/statistics/db.py
+++ dtable_events/statistics/db.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+import logging
 from hashlib import md5
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy import distinct
+
+from dtable_events.statistics.models import UserActivityStatistics
+
+logger = logging.getLogger(__name__)
 
 
 def save_user_activity_stat(session, msg):
@@ -14,3 +23,30 @@ def save_user_activity_stat(session, msg):
 
     session.execute(cmd, msg)
     session.commit()
+
+
+def get_user_activity_stats_by_day(session, start, end, offset='+00:00'):
+    start_str = start.strftime('%Y-%m-%d 00:00:00')
+    end_str = end.strftime('%Y-%m-%d 23:59:59')
+
+    start_at_0 = datetime.strptime(start_str, '%Y-%m-%d %H:%M:%S')
+    end_at_23 = datetime.strptime(end_str, '%Y-%m-%d %H:%M:%S')
+
+    try:
+        q = session.query(
+            func.date(func.convert_tz(UserActivityStatistics.timestamp, '+00:00', offset)).label("timestamp"),
+            func.count(distinct(UserActivityStatistics.username)).label("number")
+        )
+        q = q.filter(UserActivityStatistics.timestamp.between(
+            func.convert_tz(start_at_0, offset, '+00:00'), func.convert_tz(end_at_23, offset, '+00:00')
+        ))
+        rows = q.group_by(func.date(func.convert_tz(UserActivityStatistics.timestamp, '+00:00', offset))).\
+            order_by("timestamp").all()
+    except Exception as e:
+        logger.error('Get user activity statistics failed:', e)
+        rows = list()
+
+    res = list()
+    for row in rows:
+        res.append((datetime.strptime(str(row.timestamp), '%Y-%m-%d'), row.number))
+    return res
