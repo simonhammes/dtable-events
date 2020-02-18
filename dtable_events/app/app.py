3f4c9baa0e119d8eb5d53235d200bd4a2e8e697e
chengxiongchao <1223408988@qq.com>
2020-02-18 16:18:38 +0800

    ADD: dtables-cleaner

--- dtable_events/app/app.py
+++ dtable_events/app/app.py
@@ -3,6 +3,7 @@ from dtable_events.activities.handlers import MessageHandler
 from dtable_events.statistics.counter import UserActivityCounter
 from dtable_events.tasks.work_weixin_notices_sender import WorkWinxinNoticeSender
 from dtable_events.tasks.email_notices_sender import EmailNoticesSender
+from dtable_events.tasks.dtables_cleaner import DTablesCleaner
 
 
 class App(object):
@@ -11,9 +12,11 @@ class App(object):
         self._user_activity_counter = UserActivityCounter(config)
         self._work_weixin_notices_sender = WorkWinxinNoticeSender(config)
         self._email_notices_sender = EmailNoticesSender(config)
+        self._dtables_cleaner = DTablesCleaner(config)
 
     def serve_forever(self):
         self._message_handler.start()
         self._user_activity_counter.start()
         self._work_weixin_notices_sender.start()
         self._email_notices_sender.start()
+        self._dtables_cleaner.start()
---
+++ dtable_events/tasks/dtables_cleaner.py
@@ -0,0 +1,104 @@
+import os
+import logging
+from threading import Thread, Event
+
+from dtable_events.utils import get_opt_from_conf_or_env, get_python_executable, run
+
+
+# DTABLE_WEB_DIR
+dtable_web_dir = os.environ.get('DTABLE_WEB_DIR', '')
+if not dtable_web_dir:
+    logging.critical('dtable_web_dir is not set')
+    raise RuntimeError('dtable_web_dir is not set')
+if not os.path.exists(dtable_web_dir):
+    logging.critical('dtable_web_dir %s does not exist' % dtable_web_dir)
+    raise RuntimeError('dtable_web_dir does not exist')
+
+__all__ = [
+    'DTablesCleaner',
+]
+
+
+class DTablesCleaner(object):
+
+    def __init__(self, config):
+        self._enabled = False
+        self._logfile = None
+        self._interval = 60
+        self._prepare_logfile()
+        self._parse_config(config)
+
+    def _prepare_logfile(self):
+        logdir = os.path.join(os.environ.get('DTABLE_EVENTS_LOG_DIR', ''))
+        self._logfile = os.path.join(logdir, 'dtables_cleaner.log')
+
+    def _parse_config(self, config):
+        """parse dtables cleaner related options from config file
+        """
+        section_name = 'DTABLES-CLEANER'
+        key_enabled = 'enabled'
+        key_trash_interval = 'trash_interval'
+
+        if not config.has_section(section_name):
+            return
+
+        # enabled
+        enabled = get_opt_from_conf_or_env(config, section_name, key_enabled, default=False)
+        if not enabled:
+            return
+        self._enabled = True
+
+        # clean_interval
+        clean_interval = get_opt_from_conf_or_env(config, section_name, key_trash_interval, default=60*60*24*30)
+        try:
+            clean_interval = int(clean_interval)
+        except:
+            logging.warning('clean_interval value: %s is invalid.' % clean_interval)
+            clean_interval = 60 * 60 * 24 * 30
+        self._clean_interval = clean_interval
+
+    def start(self):
+        if not self.is_enabled():
+            logging.warning('Can not start dtables cleaner: it is not enabled!')
+            return
+
+        logging.info('Start dtables cleaner, interval = %s sec', self._interval)
+
+        DTablesCleanerTimer(self._interval, self._logfile, self._clean_interval).start()
+
+    def is_enabled(self):
+        return self._enabled
+
+
+class DTablesCleanerTimer(Thread):
+
+    def __init__(self, interval, logfile, 
+                clean_interval=30*60):
+        super(DTablesCleanerTimer, self).__init__()
+        self._interval = interval
+        self._logfile = logfile
+        self._clean_interval = clean_interval
+
+        self.finished = Event()
+
+    def run(self):
+        while not self.finished.is_set():
+            self.finished.wait(self._interval)
+            if not self.finished.is_set():
+                logging.info('Starts to clean trash dtables...')
+                try:
+                    python_exec = get_python_executable()
+                    manage_py = os.path.join(dtable_web_dir, 'manage.py')
+                    cmd = [
+                        python_exec,
+                        manage_py,
+                        'clean_trash_dtables',
+                        self._clean_interval,
+                    ]
+                    with open(self._logfile, 'a') as fp:
+                        run(cmd, cwd=dtable_web_dir, output=fp)
+                except Exception as e:
+                    logging.exception('error when cleaning trash dtables: %s', e)
+
+    def cancel(self):
+        self.finished.set()
