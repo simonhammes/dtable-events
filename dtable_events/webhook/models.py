113aae4fe043c56bea31c756e9a962713350a2dd
chengxiongchao <1223408988@qq.com>
2020-09-24 17:50:37 +0800

    ADD: webhooks

---
+++ dtable_events/webhook/models.py
@@ -0,0 +1,114 @@
+import json
+import logging
+from datetime import datetime
+from hashlib import sha1
+
+from sqlalchemy import Column, Integer, String, DateTime, Text, text
+
+from dtable_events.db import Base
+
+logger = logging.getLogger(__name__)
+
+
+PENDING = 0
+SENDING = 1
+SUCCESS = 2
+FAILURE = 3
+
+
+class Webhooks(Base):
+    """
+    webhooks model
+    just for read in dtable-events, so model is perhaps fragmentary
+    """
+    __tablename__ = 'webhooks'
+
+    id = Column(Integer, primary_key=True)
+    dtable_uuid = Column(String(32), nullable=False, index=True)
+    url = Column(String(2000), nullable=False)
+    settings = Column(Text, nullable=False)
+
+    @property
+    def hook_settings(self):
+        hook_settings = self.settings
+        try:
+            hook_settings = json.loads(self.settings)
+        except Exception as e:
+            return {}
+        return hook_settings
+
+    def is_event_trigger(self, event):
+        hook_settings = self.hook_settings
+        if not hook_settings:
+            return False
+        events = hook_settings.get('events', [])
+        if event in events:
+            return True
+
+    def gen_request_body(self, event):
+        """
+        must return dict
+        """
+        if event.get('event') == 'update':
+            return {
+                'event': 'update',
+                'data': event.get('data')
+            }
+        return {}
+
+    def gen_request_headers(self):
+        """
+        must return dict
+        """
+        hook_settings = self.hook_settings
+        if not hook_settings:
+            return None
+        secret = hook_settings.get('secret')
+        if not secret:
+            return None
+        return {
+            'X-SeaTable-Signature': sha1(secret.encode('utf-8')).hexdigest()
+        }
+
+class WebhookJobs(Base):
+    """
+    webhook_jobs model
+    """
+    __tablename__ = 'webhook_jobs'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    webhook_id = Column(String(length=36), index=True, nullable=False)
+    created_at = Column(DateTime)
+    trigger_at = Column(DateTime)
+    status = Column(Integer, default=0, index=True)
+    url = Column(String(2000), nullable=False)
+    request_headers = Column(Text)
+    request_body = Column(Text)
+    response_status = Column(Integer)
+    response_body = Column(Text)
+
+    def __init__(self, webhook_id, requst_body, url, request_headers=None, status=PENDING):
+        self.webhook_id = webhook_id
+        self.url = url
+        self.request_body = json.dumps(requst_body) if isinstance(requst_body, dict) else str(requst_body)
+        self.created_at = datetime.now()
+        if request_headers:
+            self.request_headers = json.dumps(request_headers) if isinstance(request_headers, dict) else str(request_headers)
+        if status:
+            self.status = status
+
+
+class DB:
+    def __init__(self, session):
+        self.session = session
+
+    def __enter__(self):
+        return self.session
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.session.commit()
+        except Exception as e:
+            logger.error(e)
+        finally:
+            self.session.close()
