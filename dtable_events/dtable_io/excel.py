1ce707b67d8118fd937e97a7bc038bebaab486a4
skywalker <dzmbbs@qq.com>
2021-01-14 15:30:41 +0800

    head_index

--- dtable_events/dtable_io/excel.py
+++ dtable_events/dtable_io/excel.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from datetime import datetime
 
 from openpyxl import load_workbook
@@ -18,85 +19,101 @@ CHECKBOX_STRING_LIST = [string for item in CHECKBOX_TUPLE for string in item]
 CHECKBOX_TRUE_LISt = [item[0] for item in CHECKBOX_TUPLE]
 
 
-def parse_excel_rows(sheet_rows, columns, max_row, max_column):
+def parse_excel_rows(sheet_rows, columns, head_index, max_column):
+    if head_index < 0:
+        value_rows = sheet_rows
+    else:
+        value_rows = sheet_rows[head_index + 1:]
     rows = []
-    for row in sheet_rows[1:]:
+    for row in value_rows:
         row_data = {}
         for index in range(max_column):
-            cell_value = row[index].value
-            column_name = columns[index]['name']
-            column_type = columns[index]['type']
-            if cell_value is None:
-                continue
-            elif column_type == 'number':
-                row_data[column_name] = cell_value
-            elif column_type == 'date':
-                row_data[column_name] = str(cell_value)
-            elif column_type == 'long-text':
-                row_data[column_name] = {
-                    'preview': cell_value[:10].replace('\n', ' '),
-                    'text': cell_value,
-                }
-            elif column_type == 'checkbox':
-                row_data[column_name] = True if cell_value in CHECKBOX_TRUE_LISt else False
-            elif column_type == 'multiple-select':
-                multiple_value = cell_value.split('，') if '，' in cell_value else cell_value.split(',')
-                row_data[column_name] = [value.strip(' ') for value in multiple_value]
-            else:
-                row_data[column_name] = str(cell_value)
+            try:
+                cell_value = row[index].value
+                column_name = columns[index]['name']
+                column_type = columns[index]['type']
+                if cell_value is None:
+                    continue
+                elif column_type == 'number':
+                    row_data[column_name] = cell_value
+                elif column_type == 'date':
+                    row_data[column_name] = str(cell_value)
+                elif column_type == 'long-text':
+                    row_data[column_name] = {
+                        'preview': cell_value[:10].replace('\n', ' '),
+                        'text': cell_value,
+                    }
+                elif column_type == 'checkbox':
+                    row_data[column_name] = True if cell_value in CHECKBOX_TRUE_LISt else False
+                elif column_type == 'multiple-select':
+                    multiple_value = cell_value.split('，') if '，' in cell_value else cell_value.split(',')
+                    row_data[column_name] = [value.strip(' ') for value in multiple_value]
+                else:
+                    row_data[column_name] = str(cell_value)
+            except Exception as e:
+                logging.exception(e)
         rows.append(row_data)
 
     return rows
 
 
 def parse_excel_column_type(value_list):
-    type_list = []
-    # Check the first 200 rows of data
-    for cell_value in value_list[:200]:
-        if cell_value is None:
-            continue
-        elif isinstance(cell_value, int) or isinstance(cell_value, float):
-            column_type = 'number'
-        elif isinstance(cell_value, datetime):
-            column_type = 'date'
-        elif '\n' in cell_value:
-            column_type = 'long-text'
-        elif cell_value in CHECKBOX_STRING_LIST:
-            column_type = 'checkbox'
-        elif ',' in cell_value or '，' in cell_value:
-            column_type = 'multiple-select'
-            multiple_value = cell_value.split('，') if '，' in cell_value else cell_value.split(',')
-            for value in multiple_value:
-                if len(value.strip(' ')) > 20:
-                    # more than 20 characters.
-                    column_type = 'text'
-        else:
-            column_type = 'text'
-        type_list.append(column_type)
-
-    max_column_type = max(type_list, key=type_list.count) if type_list else 'text'
-    column_data = None
-    if max_column_type == 'multiple-select':
-        multiple_list = []
-        for cell_value in value_list:
+    try:
+        type_list = []
+        # Check the first 200 rows of data
+        for cell_value in value_list[:200]:
             if cell_value is None:
                 continue
-            multiple_value = cell_value.split('，') if '，' in cell_value else cell_value.split(',')
-            for value in multiple_value:
-                value = value.strip(' ')
-                if value not in multiple_list:
-                    multiple_list.append(value)
-        column_data = {'options': [{'name': value} for value in multiple_list]}
+            elif isinstance(cell_value, int) or isinstance(cell_value, float):
+                column_type = 'number'
+            elif isinstance(cell_value, datetime):
+                column_type = 'date'
+            elif '\n' in cell_value:
+                column_type = 'long-text'
+            elif cell_value in CHECKBOX_STRING_LIST:
+                column_type = 'checkbox'
+            elif (',' in cell_value or '，' in cell_value) \
+                    and ('{' not in cell_value):
+                column_type = 'multiple-select'
+                multiple_value = cell_value.split('，') if '，' in cell_value else cell_value.split(',')
+                for value in multiple_value:
+                    if len(value.strip(' ')) > 20:
+                        # more than 20 characters.
+                        column_type = 'text'
+            else:
+                column_type = 'text'
+            type_list.append(column_type)
 
-    return max_column_type, column_data
+        max_column_type = max(type_list, key=type_list.count) if type_list else 'text'
+        column_data = None
+        if max_column_type == 'multiple-select':
+            multiple_list = []
+            for cell_value in value_list:
+                if cell_value is None:
+                    continue
+                multiple_value = cell_value.split('，') if '，' in cell_value else cell_value.split(',')
+                for value in multiple_value:
+                    value = value.strip(' ')
+                    if value not in multiple_list:
+                        multiple_list.append(value)
+            column_data = {'options': [{'name': value} for value in multiple_list]}
 
+        return max_column_type, column_data
+    except Exception as e:
+        logging.exception(e)
+        return 'text', None
 
-def parse_excel_columns(sheet_rows, max_row, max_column):
-    first_row = sheet_rows[0]
-    value_rows = sheet_rows[1:]
+def parse_excel_columns(sheet_rows, head_index, max_column):
+    if head_index == -1:
+        head_row = [None] * head_index
+        value_rows = sheet_rows
+    else:
+        head_row = sheet_rows[head_index]
+        value_rows = sheet_rows[head_index + 1:]
+
     columns = []
     for index in range(max_column):
-        name = first_row[index].value
+        name = head_row[index].value
         column_name = str(name) if name else 'Field' + str(index + 1)
         value_list = [row[index].value for row in value_rows]
         column_type, column_data = parse_excel_column_type(value_list)
@@ -118,9 +135,10 @@ def parse_excel_to_json(repo_id, dtable_name, custom=False):
     # user custom columns
     if custom:
         json_file = get_excel_json_file(repo_id, dtable_name)
-        table_columns_map = {table['name']: table['columns']for table in json.loads(json_file)}
+        tables = json.loads(json_file)
+        head_index_map = {table['name']: table['head_index'] for table in tables}
     else:
-        table_columns_map = {}
+        head_index_map = {}
 
     # parse
     excel_file = get_excel_file(repo_id, dtable_name)
@@ -133,18 +151,23 @@ def parse_excel_to_json(repo_id, dtable_name, custom=False):
             continue
 
         if custom:
-            columns = table_columns_map.get(sheet.title, [])
-            max_column = len(columns)
+            head_index = head_index_map.get(sheet.title, 0)
+            if head_index > max_row - 1:
+                head_index = 0
         else:
-            max_column = sheet.max_column
-            columns = parse_excel_columns(sheet_rows, max_row, max_column)
+            head_index = 0
 
-        rows = parse_excel_rows(sheet_rows, columns, max_row, max_column)
+        max_column = sheet.max_column
+        columns = parse_excel_columns(sheet_rows, head_index, max_column)
+        rows = parse_excel_rows(sheet_rows, columns, head_index, max_column)
 
         table = {
             'name': sheet.title,
             'rows': rows,
             'columns': columns,
+            'head_index': head_index,
+            'max_row': max_row,
+            'max_column': max_column,
         }
         tables.append(table)
 
