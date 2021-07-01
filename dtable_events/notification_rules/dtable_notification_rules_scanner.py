e331ba77b6857bc710f83e72120cfdfd48babc8f
AlexCXC <1223408988@qq.com>
2021-07-01 17:36:02 +0800

    update comment

--- dtable_events/notification_rules/dtable_notification_rules_scanner.py
+++ dtable_events/notification_rules/dtable_notification_rules_scanner.py
@@ -141,7 +141,7 @@ class DTableNotificationRulesCleaner(Thread):
 
     def run(self):
         sched = BlockingScheduler()
-        # fire at every hour in every day of week
+        # fire at 0 o'clock in every day of week
         @sched.scheduled_job('cron', day_of_week='*', hour='0')
         def timed_job():
             logging.info('Starts to clean inactive notification rules...')
