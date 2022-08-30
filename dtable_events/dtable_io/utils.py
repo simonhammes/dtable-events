e9491c56b3a57246a9265f807c85cd9ecb58603c
JoinTyang <41655440+JoinTyang@users.noreply.github.com>
2022-08-30 13:54:44 +0800

    fix update table urlencode bug (#319)

--- dtable_events/dtable_io/utils.py
+++ dtable_events/dtable_io/utils.py
@@ -729,7 +729,7 @@ def append_excel_json_to_dtable_server(username, dtable_uuid, json_file, table_n
 
 def get_columns_from_dtable_server(username, dtable_uuid, table_name):
     api_url = get_inner_dtable_server_url()
-    url = api_url.rstrip('/') + '/api/v1/dtables/' + dtable_uuid + '/columns/?from=dtable_events&table_name=' + table_name
+    url = api_url.rstrip('/') + '/api/v1/dtables/' + dtable_uuid + '/columns/?from=dtable_events&table_name=' + urlquote(table_name)
     dtable_server_access_token = get_dtable_server_token(username, dtable_uuid)
     headers = {'Authorization': 'Token ' + dtable_server_access_token}
 
@@ -758,7 +758,7 @@ def get_csv_file(repo_id, file_name):
 
 def get_rows_from_dtable_server(username, dtable_uuid, table_name):
     api_url = get_inner_dtable_server_url()
-    url = api_url.rstrip('/') + '/api/v1/dtables/' + dtable_uuid + '/rows/?' + 'table_name=' + table_name + \
+    url = api_url.rstrip('/') + '/api/v1/dtables/' + dtable_uuid + '/rows/?table_name=' + urlquote(table_name) + \
           '&convert_link_id=true&from=dtable_events'
     dtable_server_access_token = get_dtable_server_token(username, dtable_uuid)
     headers = {'Authorization': 'Token ' + dtable_server_access_token}
