aa53a0b52ea2e9b2c8100acea5ea72e2465690f4
杨国璇 <37972689+er-pai-r@users.noreply.github.com>
2022-12-21 14:00:46 +0800

    fix: statistic table include me (#393)
    * fix: statistic table include me
    
    * feat: update code

--- dtable_events/utils/sql_generator.py
+++ dtable_events/utils/sql_generator.py
@@ -1125,6 +1125,7 @@ class StatisticSQLGenerator(object):
         summary_column_key = self.statistic.get('summary_column_key', '')
         groupby_date_granularity = self.statistic.get('groupby_date_granularity', '')
         groupby_geolocation_granularity = self.statistic.get('groupby_geolocation_granularity', '')
+        groupby_include_empty_cells = self.statistic.get('groupby_include_empty_cells', False)
         summary_method = self.statistic.get('summary_method', '')
         summary_columns = self.statistic.get('summary_columns', [])
         if not summary_method:
@@ -1136,6 +1137,7 @@ class StatisticSQLGenerator(object):
             self.error = 'Group by column not found'
             return ''
         
+        self._update_filter_sql(groupby_include_empty_cells, groupby_column)
         groupby_column_name = self._statistic_column_name_to_sql(groupby_column, { 'date_granularity': groupby_date_granularity, 'geolocation_granularity': groupby_geolocation_granularity })
         summary_type = summary_type.upper()
 
@@ -1188,6 +1190,7 @@ class StatisticSQLGenerator(object):
         summary_column_key = self.statistic.get('summary_column_key', '')
         groupby_date_granularity = self.statistic.get('groupby_date_granularity', '')
         groupby_geolocation_granularity = self.statistic.get('groupby_geolocation_granularity', '')
+        groupby_include_empty_cells = self.statistic.get('groupby_include_empty_cells', False)
         column_groupby_date_granularity = self.statistic.get('column_groupby_date_granularity', '')
         column_groupby_geolocation_granularity = self.statistic.get('column_groupby_geolocation_granularity', '')
         summary_method = self.statistic.get('summary_method', '')
@@ -1201,6 +1204,7 @@ class StatisticSQLGenerator(object):
             self.error = 'Group by column not found'
             return ''
 
+        self._update_filter_sql(groupby_include_empty_cells, groupby_column)
         groupby_column_name = self._statistic_column_name_to_sql(groupby_column, { 'date_granularity': groupby_date_granularity, 'geolocation_granularity': groupby_geolocation_granularity })
         column_groupby_column_name = self._statistic_column_name_to_sql(column_groupby_column, { 'date_granularity': column_groupby_date_granularity, 'geolocation_granularity': column_groupby_geolocation_granularity })
 
@@ -1228,12 +1232,14 @@ class StatisticSQLGenerator(object):
         summary_column_key = self.statistic.get('summary_column_key', '')
         groupby_date_granularity = self.statistic.get('groupby_date_granularity', '')
         groupby_geolocation_granularity = self.statistic.get('groupby_geolocation_granularity', '')
+        groupby_include_empty_cells = self.statistic.get('groupby_include_empty_cells', False)
         
         groupby_column = self._get_column_by_key(groupby_column_key)
         if not groupby_column:
             self.error = 'Group by column not found'
             return ''
         
+        self._update_filter_sql(groupby_include_empty_cells, groupby_column)
         groupby_column_name = self._statistic_column_name_to_sql(groupby_column, { 'date_granularity': groupby_date_granularity, 'geolocation_granularity': groupby_geolocation_granularity })
         summary_type = summary_type.upper()
         if summary_type == 'COUNT':
