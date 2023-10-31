26db41e22f028ae80b9a6d42b609f5689b5b7892
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2023-10-31 12:12:41 +0800

    Add format when export view excel (#551)
    * Update excel.py
    
    * Update excel.py

--- dtable_events/dtable_io/excel.py
+++ dtable_events/dtable_io/excel.py
@@ -1493,7 +1493,13 @@ def handle_row(row, row_num, ws, email2nickname, unknown_user_set, unknown_cell_
             except Exception as e:
                 c = WriteOnlyCell(ws, value=None)
             else:
-                c.number_format = gen_decimal_format(cell_value)
+                column_data = column.get('data')
+                if column_data:
+                    formula_value, number_format = parse_formula_number(cell_value, column.get('data'))
+                    c.number_format = number_format
+                else:
+                    c.number_format = gen_decimal_format(cell_value)
+
         elif col_type == ColumnTypes.DATE:
             c = WriteOnlyCell(ws, value=format_time(cell_value))
             if column.get('data'):
